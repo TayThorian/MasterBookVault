64bb9b54f3264d4587e9899b3a392ef965d28ebd
TayThorian <40669589+TayThorian@users.noreply.github.com>
2024-05-04 09:13:56 +0100

    Updates

--- MasterBookVault/MasterBookVault/obj/Debug/net7.0-windows/MasterBookVault.AssemblyInfo.vb
+++ MasterBookVault/MasterBookVault/obj/Debug/net7.0-windows/MasterBookVault.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("MasterBookVault"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4adc60c9385f158a387a75c8fb227221bebd46a"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1399538907a972fe1cb417c9bd4b6039c8e4ed9a"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("MasterBookVault"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("MasterBookVault"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
